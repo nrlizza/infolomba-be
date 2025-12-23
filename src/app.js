5d1ad8426d4a57e9d03a7b26c5a50835ea3c4585
nrlizza <nurul.izza@students.paramadina.ac.id>
2025-12-23 13:08:47 +0700

    [UPDATE] add allowed origin

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 
 app.use(helmet());
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'https://infolomba-fe.vercel.app'];
 
 app.use(cors({
   origin: (origin, callback) => {
